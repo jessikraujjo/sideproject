acde2bddd4594664d141a8c7460872bec0a1ecb1
jessikraujjo <jessikraujjo@gmail.com>
2017-05-22 15:24:40 -0300

    Initial Prototype

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>ponei</title>
+		<link  rel="stylesheet" type="text/css" href="estilos.css">
+	</head>
+	<body>
+		<h2>#desafio100videos </h2>
+		 <h2>Faltam 13 vídeos</h2>
+		 <?php
+		 	for ($i=0; $i < 2 ; $i++) { 
+		 		?><div class="retangulo"></div><?php
+		 	}
+		 ?>
+		
+	</body>
+</html>
